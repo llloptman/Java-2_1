c306d9ab18c7e7ea6299fdff832ac3a35a36cb02
NeuroStartUp worker 1 <NeoruStartUp@worker1.com>
2020-08-20 11:33:35 +0300

    init Main Java

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int curentAmmount = 2_000_000_000;
+        int transition = 500_000_000;
+        int totalAmmount = curentAmmount + transition;
+
+        System.out.println(totalAmmount);
+    }
+}
